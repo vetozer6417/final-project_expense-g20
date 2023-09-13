0c15ed1cdf67a94be7aef5f4e34ca1f10dc79c96
vetozer6417 <vetozer6417@gmail.com>
2023-09-14 00:35:38 +0700

    api connected

---
+++ backend/server.js
@@ -0,0 +1,62 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+const mongoose = require('mongoose')
+
+const app = express();
+const PORT = process.env.PORT || 3000
+
+mongoose.connect('mongodb+srv://admin:admin@final-project.fnwl9ey.mongodb.net/?retryWrites=true&w=majority', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+const Transaction = mongoose.model('Transaction', {
+    type: String,
+    amount: Number,
+    date: Date,
+    note: String,
+});
+
+app.use(bodyParser.json());
+
+app.get('/api/transactions', async (req, res) => {
+    try {
+        // Find all transactions in the database
+        const transactions = await Transaction.find();
+    
+        // Respond with the list of transactions
+        res.json(transactions);
+      } catch (error) {
+        console.error('Error fetching transactions:', error);
+        res.status(500).json({ error: 'An error occurred while fetching transactions' });
+      }
+})
+
+// Define a route to handle POST requests to add transactions
+app.post('/api/transactions', async (req, res) => {
+  try {
+    // Extract transaction data from the request body
+    const { type, amount, date, note } = req.body;
+
+    // Create a new transaction document and save it to the database
+    const transaction = new Transaction({
+      type,
+      amount,
+      date: new Date(date), // Convert date string to Date object
+      note,
+    });
+
+    await transaction.save();
+
+    // Respond with a success message or the saved transaction data
+    res.json({ message: 'Transaction added successfully', transaction });
+  } catch (error) {
+    console.error('Error adding transaction:', error);
+    res.status(500).json({ error: 'An error occurred while adding the transaction' });
+  }
+});
+
+// Start the server
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
---
+++ frontend/script.js
@@ -0,0 +1,44 @@
+const input_type = document.getElementById("input-type")
+const input_amount = document.getElementById("input-amount")
+const input_date = document.getElementById("input-date")
+const input_note = document.getElementById("input-note")
+const input_form = document.getElementById("form-container")
+
+//add transaction to database
+async function addTransaction(event) {
+    event.preventDefault()
+    if(input_amount.value.trim() === '' || input_date.value.trim() === '' ) {
+        alert('Please fill in all the information!')
+        return
+    } 
+    
+    const new_transaction = {
+        type: input_type.value,
+        amount: parseFloat(input_amount.value),
+        date: input_date.value,
+        note: input_note.value
+    }
+
+    fetch('http://localhost:3000/api/transactions', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(new_transaction)
+    })
+    .then(response => response.json())
+    .then(data => {
+        // Handle the response from your server, e.g., display a success message
+        console.log('Transaction added successfully:', data);
+
+        // Optionally, clear the form fields
+        input_type.value = 'expense';
+        input_amount.value = '';
+        input_date.value = '';
+        input_note.value = '';
+    })
+}
+
+input_form.addEventListener('submit',addTransaction)
+
+//transaction display
